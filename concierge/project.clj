8ec2d8cb0d8766d17726c28fa072bd23325634ab
Thomas Vladeck <thomas.vladeck@gmail.com>
2012-07-22 20:00:02 -0700

    added mahout to project.clj

--- concierge/project.clj
+++ concierge/project.clj
@@ -1,8 +1,6 @@
 (defproject concierge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [incanter "1.2.3-SNAPSHOT"]
-                 [org.apache.mahout/mahout-core "0.5"]
-                 [org.apache.mahout/mahout-math "0.5"]
-                 [org.apache.mahout/mahout-utils "0.5"]               
-                 ])
+                 [org.apache.mahout/mahout-core "0.7"]
+                 [org.apache.mahout/mahout-math "0.7"]
+                [org.apache.mahout/mahout-utils "0.5"]])
