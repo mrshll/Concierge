806d22a94dd14bb1af27d51339d0bb42e4685d5b
Andrew McGlathery <a.mcglathery@gmail.com>
2012-10-22 18:48:05 -0400

    finished getCuisineVector() in Restaurant.java

--- learn/Restaurant.java
+++ learn/Restaurant.java
@@ -14,8 +14,42 @@ class Restaurant {
      * it does not
      */
     public ArrayList<Double> getCuisineVector(){
-        //TODO: How can this be done?
+        String[] possibleCuisines = new String[] { "Afghan", "American",
+            "Argentine", "Asian", "Austrian", "Bagels", "Bakery", "Barbecue",
+            "Belgian", "Bistro", "Brazilian", "British", "Buffet", "Burgers",
+            "Cafe", "Cajun", "Californian", "Calzones", "Cambodian",
+            "Caribbean", "Catering", "Cheesesteaks", "Chicken", "Chinese",
+            "Chowder", "Coffee", "Colombian", "Contemporary", "Continental",
+            "Creole", "Crepes", "Cuban", "Czech", "Deli", "Dim Sum", "Diner",
+            "Dominican", "Donuts", "Eastern European", "Eclectic", "English",
+            "Ethiopian", "European", "Fast Food", "Filipino", "Fish and Chips",
+            "Fondue", "French", "Frozen Yogurt", "Fusion", "Gastropub",
+            "German", "Greek", "Grill", "Gyros", "Haitian", "Halal", "Hawaiian",
+            "Healthy", "Hookah Bar", "Hot Dogs", "Ice Cream", "Indian",
+            "Indonesian", "International", "Irish", "Israeli", "Italian",
+            "Japanese", "Juices", "Korean", "Korean Barbeque", "Kosher",
+            "Latin", "Latin American", "Lebanese", "Malaysian", "Mediterranean",
+            "Mexican", "Middle Eastern", "Mongolian", "Moroccan", "Nepalese",
+            "Noodle Bar", "Norwegian", "Organic", "Oysters", "Pacific Rim",
+            "Pakistani", "Pan Asian", "Pasta", "Pasteries", "Persian",
+            "Peruvian", "Pho", "Pizza", "Polish", "Polynesian", "Portuguese",
+            "Pub Food", "Puerto Rican", "Ribs", "Salad", "Salvadoran",
+            "Sandwiches", "Seafood", "Singaporean", "Smoothies", "Soul Food",
+            "Soup", "South American", "South Pacific", "Southern",
+            "Southwestern", "Spanish", "Steak", "Subs", "Sushi", "Taiwanese",
+            "Tapas", "Tea", "Tex Mex", "Thai", "Tibetan", "Traditional",
+            "Turkish", "Ukrainian", "Vegan", "Vegetarian", "Venezuelan",
+            "Venusian", "Vietnamese", "Wings", "Wraps"};
+
+
         ArrayList<Double> vector = new ArrayList<Double>();
+        for (int i = 0; i < possibleCuisines.length; i++){
+            if (cuisines.contains(possibleCuisines[i])){
+                vector.add(new Double(1.0));
+            } else {
+                vector.add(new Double(0.0));
+            }
+        }
         return vector;
     }
 
