9b37fcfd865f3c0649f0a96fbd5ec1bf1d256711
Marshall <mmoutenot@gmail.com>
2012-10-24 20:33:27 -0400

    add models

---
+++ web/utility/models.py
@@ -0,0 +1,36 @@
+from south.modelsinspector import add_introspection_rules
+from django.db import models
+
+add_introspection_rules([], ["^utility\.models\.IntegerRangeField"])
+add_introspection_rules([], ["^utility\.models\.SeparatedBaluesTimeField"])
+
+# Create your models here.
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
+class SeparatedValuesTimeField(models.TextField):
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        self.token = kwargs.pop('token', ',')
+        super(SeparatedValuesTimeField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value: return
+        if isinstance(value, list):
+            return value
+        return value.split(self.token)
+
+    def get_db_prep_value(self, value):
+        if not value: return
+        assert(isinstance(value, list) or isinstance(value, tuple))
+        return self.token.join([unicode(s) for s in value])
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
