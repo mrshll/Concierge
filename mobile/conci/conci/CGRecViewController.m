598b9aa80d176964f5142f2d6553a6922ead80cd
Marshall <mmoutenot@gmail.com>
2012-10-27 16:18:53 -0400

    add migrations;

---
+++ mobile/conci/conci/CGRecViewController.m
@@ -0,0 +1,33 @@
+//
+//  CGFirstViewController.m
+//  conci
+//
+//  Created by Marshall Moutenot on 10/27/12.
+//  Copyright (c) 2012 Marshall Moutenot. All rights reserved.
+//
+
+#import "Constants.h"
+#import "CGCommunication.h"
+#import "CGRecViewController.h"
+
+@interface CGFirstViewController ()
+
+@end
+
+@implementation CGFirstViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  // start asking the server api for a list of recs
+  
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
