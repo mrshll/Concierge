d30ef4d1b8c7f1990363dd4f5676ee2f724395c2
Marshall <mmoutenot@gmail.com>
2012-10-30 15:23:49 -0400

    trying to avoid the json error

--- web/concierge/views.py
+++ web/concierge/views.py
@@ -18,8 +18,12 @@ def index(request, template='index.html'):
         print(request.user)
         user_profile = request.user.get_profile()
         print(user_profile.profiles)
-        # We replace single quotes with double quotes b/c of python's strict json requirements
-        profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
+
+        if user_profile.profiles and user_profile.profiles != "":
+          # We replace single quotes with double quotes b/c of python's strict json requirements
+          profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
+        else:
+          profiles = ""
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
         )
