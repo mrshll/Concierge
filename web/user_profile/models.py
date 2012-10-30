69d01dd2bb1f753ce399e5c48bf9ad31c2f6c0ca
Marshall <mmoutenot@gmail.com>
2012-10-30 01:05:55 -0400

    user_profile has favorites and accessor methods for average spending and most popluar cuisines

--- web/user_profile/models.py
+++ web/user_profile/models.py
@@ -1,17 +1,37 @@
+from collections import Counter
+
 from django.contrib.auth.models import User
 from django.db import models
 from managers import UserProfileManager
 
-from recommendation_item import models
+from recommendation_item.models import Restaurant
 
+# indicates a users initial preferences
+class Favorite(models.Model):
+  restaurant = models.ForeignKey(Restaurant)
 
 class UserProfile(models.Model):
-    access_token = models.CharField(max_length=260, null=True, blank=True)
-    singly_id = models.CharField(max_length=260, null=True, blank=True)
-    profiles = models.TextField(null=True, blank=True)
-    user = models.ForeignKey(User, related_name='profile')
+  access_token = models.CharField(max_length=260, null=True, blank=True)
+  singly_id = models.CharField(max_length=260, null=True, blank=True)
+  profiles = models.TextField(null=True, blank=True)
+  favorites = models.ManyToManyField(Favorite)
+  user = models.ForeignKey(User, related_name='profile')
 
-    objects = UserProfileManager()
+  objects = UserProfileManager()
 
-    class Meta:
-        db_table = 'user_profile'
+  class Meta:
+    db_table = 'user_profile'
+
+  def get_avg_price (self):
+    return sum([f.restaurant.price for f in self.favorites])/len(self.favorites)
+
+  # returns a dictionary with cuisine and the frequency from the list of user's
+  # favorites
+  def get_fav_cuisines (self):
+    cuisines = [f.restaurant.cuisine for f in self.favorites]
+    return Counter(cuisines)
+
+
+
+
+
