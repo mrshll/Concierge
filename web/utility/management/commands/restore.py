17924f8e0ab0de343e553d7140fb418031832e7e
Marshall <mmoutenot@gmail.com>
2012-10-31 12:35:27 -0400

    adding favorites

--- web/utility/management/commands/restore.py
+++ web/utility/management/commands/restore.py
@@ -38,53 +38,42 @@ class Command(BaseCommand):
         raise
     print(candidate_list)
 
-    key_list = map(lambda x: x, candidate_list)
-    key_list.sort(key=operator.attrgetter('key'), reverse=True)
-    logging.warning('%d candidate dumps found' % len(key_list))
-    pattern = re.compile("^%s(\d{14}).gz$" % _BACKUP_S3_DUMPFILE_PREFIX)
-    (dump_date, dump_key) = (None, None)
-    for key in key_list:
-      match = pattern.match(key.key)
-      if match:
-        dump_date = match.group(1)
-        dump_key = key
-        break
-    logging.warning("Downloading dump from '%s'" % dump_date)
+    candidate_list = sorted(candidate_list)
+    print('%d candidate dumps found' % len(candidate_list))
+    print('going with: ' + str(candidate_list[0]))
+    dumpfile_name = candidate_list[0]
     tempdir_name = tempfile.mkdtemp()
     if sys.platform == 'win32':
       tempfile_name = "%s\\\\%ssql" % (tempdir_name, _BACKUP_DUMPFILE_PREFIX)
     else:
       tempfile_name = "%s/%ssql" % (tempdir_name, _BACKUP_DUMPFILE_PREFIX)
 
-    try:
-      # this is a hack method, should stream this
-      # all to avoid using zcat for windows
-      compressed_string = dump_key.get_contents_as_string()
-      compressed_stream = StringIO(compressed_string)
-      gzipper = gzip.GzipFile(fileobj=compressed_stream, mode='rb')
-      out_file = file(tempfile_name, mode='wb')
-      for line in gzipper:
-        out_file.write(line)
-      out_file.close()
-      gzipper.close()
-      compressed_stream.close()
-      host_flag = ''
-      if settings.DATABASES['default']['HOST']:
-        host_flag = "-h %s" % settings.DATABASES['default']['HOST']
-      run_command("mysql %s -u %s -p%s %s < %s" %
-            (
-              host_flag,
-              settings.DATABASES['default']['USER'],
-              settings.DATABASES['default']['PASSWORD'],
-              settings.DATABASES['default']['NAME'],
-              tempfile_name,
-              ))
-      logging.warning("Dump from '%s' restored." % dump_date)
-      management.call_command('syncdb')
-      if not options.get('restore_only'):
-        management.call_command('migrate')
-    except Exception, e:
-      logging.error(e)
+    ftp = FTP(FTP_HOST, FTP_USER, FTP_PASS)
+    ftp.retrbinary('RETR ' + dumpfile_name, open(tempfile_name, 'wb'))
+    # this is a hack method, should stream this
+    # all to avoid using zcat for windows
+    gzipper = gzip.GzipFile(fileobj=tempfile_name, mode='rb')
+    out_file = file(tempfile__name, mode='wb')
+    for line in gzipper:
+      out_file.write(line)
+    out_file.close()
+    gzipper.close()
+    compressed_stream.close()
+    host_flag = ''
+    if settings.DATABASES['default']['HOST']:
+      host_flag = "-h %s" % settings.DATABASES['default']['HOST']
+    run_command("mysql %s -u %s -p%s %s < %s" %
+          (
+            host_flag,
+            settings.DATABASES['default']['USER'],
+            settings.DATABASES['default']['PASSWORD'],
+            settings.DATABASES['default']['NAME'],
+            tempfile_name,
+            ))
+    print("Dump from '%s' restored." % dump_date)
+    management.call_command('syncdb')
+    if not options.get('restore_only'):
+      management.call_command('migrate')
 
     # always delete command
     run_command('rm -f %s' % tempfile_name)
