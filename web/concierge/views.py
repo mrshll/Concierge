2673b656f132372c9a455163dd1a88a79911a4ac
Andrew McGlathery <a.mcglathery@gmail.com>
2012-11-06 15:57:08 -0500

    moved preliminary survey to main page and added list of favorites

--- web/concierge/views.py
+++ web/concierge/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.http import HttpResponseRedirect
 import simplejson
+
+from survey.forms import UserSurvey
+from user_profile.models import Favorite
 
 
 def index(request, template='index.html'):
@@ -17,7 +21,23 @@ def index(request, template='index.html'):
     if request.user.is_authenticated():
         print(request.user)
         user_profile = request.user.get_profile()
+        user_favorites = user_profile.favorites.all()
+        user_favorites_restaurants = [f.restaurant for f in user_favorites]
         print(user_profile.profiles)
+        if request.method == "POST":
+          form = UserSurvey(request.POST)
+          if form.is_valid():
+            # process data
+            favorites = form.cleaned_data['favorites']
+            for r in favorites:
+              if r not in user_favorites_restaurants:
+                f = Favorite(restaurant=r)
+                f.save()
+                user_profile.favorites.add(f)
+            user_profile.save()
+            return HttpResponseRedirect("/")
+        else:
+          form = UserSurvey()
 
         if user_profile.profiles and user_profile.profiles != "":
           # We replace single quotes with double quotes b/c of python's strict json requirements
