7d5674a7783c8e8517ea6651aab29efef62c3331
Marshall <mmoutenot@gmail.com>
2012-11-06 16:16:07 -0500

    merging

---
+++ mobile/conci/conci/CGLocationController.m
@@ -0,0 +1,13 @@
+//
+//  CGLocationController.m
+//  conci
+//
+//  Created by Marshall Moutenot on 10/31/12.
+//  Copyright (c) 2012 Marshall Moutenot. All rights reserved.
+//
+
+#import "CGLocationController.h"
+
+@implementation CGLocationController
+
+@end
