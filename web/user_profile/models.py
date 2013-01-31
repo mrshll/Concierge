8578366d63851581747f7c95620b3f026786760f
Marshall <mmoutenot@gmail.com>
2013-01-31 14:54:50 -0500

    update user model

--- web/user_profile/models.py
+++ web/user_profile/models.py
@@ -4,36 +4,51 @@ from django.contrib.auth.models import User
 from django.db import models
 from managers import UserProfileManager
 
-from recommendation_item.models import Restaurant
+# from recommendation_item.models import Restaurant
 
 # indicates a users initial preferences
-class Favorite(models.Model):
-  restaurant = models.ForeignKey(Restaurant)
+# class Favorite(models.Model):
+#   restaurant = models.ForeignKey(Restaurant)
 
 class UserProfile(models.Model):
   access_token = models.CharField(max_length=260, null=True, blank=True)
   singly_id = models.CharField(max_length=260, null=True, blank=True)
   profiles = models.TextField(null=True, blank=True)
-  favorites = models.ManyToManyField(Favorite, null=True, blank=True)
+  # favorites = models.ManyToManyField(Favorite, null=True, blank=True)
   user = models.ForeignKey(User, related_name='profile')
+
+  # static Facebook vector
+  """
+  Where you live
+  Schools
+  Birthday/Age
+  Gender
+  Sexual orientation
+  Languages
+  Likes
+  (Count for each like category)
+  Map tags
+  Friend Count
+  """
+  fb_data = models.CharField(max_length=4000)
 
   objects = UserProfileManager()
 
   class Meta:
     db_table = 'user_profile'
 
-  def get_avg_price (self):
-    return sum([f.restaurant.price for f in
-               self.favorites.all()])/len(self.favorites.all())
+  # def get_avg_price (self):
+  #   return sum([f.restaurant.price for f in
+  #              self.favorites.all()])/len(self.favorites.all())
 
   # returns a dictionary with cuisine and the frequency from the list of user's
   # favorites
-  def get_fav_cuisines (self):
-    cuisines = []
-    for f in self.favorites.all():
-      f_cuisines = f.restaurant.cuisines.split(",")
-      [cuisines.append(cuisine) for cuisine in f_cuisines]
-    return Counter(cuisines)
+  # def get_fav_cuisines (self):
+  #   cuisines = []
+  #   for f in self.favorites.all():
+  #     f_cuisines = f.restaurant.cuisines.split(",")
+  #     [cuisines.append(cuisine) for cuisine in f_cuisines]
+  #   return Counter(cuisines)
 
 
 
