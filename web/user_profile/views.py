6f511b9b0d040b06ae88438921d828efb841c9d3
baturay <baturay.akaslan@tufts.edu>
2013-02-12 15:09:32 -0500

    Adding facebook profile integration.

--- web/user_profile/views.py
+++ web/user_profile/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 from singly import SinglyHelper
 from models import UserProfile
+from utility import fb_utility
 
 # from recommendation_item.models import Restaurant
 # from recommendation_item.api import RestaurantResource
@@ -19,6 +20,8 @@ def authorize_callback(request):
   code = request.GET.get('code')
   content = SinglyHelper.get_access_token(code)
   user_profile = UserProfile.objects.get_or_create_user(content['account'], content['access_token'])
+  user_profile.fb_data = fb_utility.fill_profile(user_profile, content['account'], content['access_token'])
+  user_profile.save()
   if not request.user.is_authenticated():
     user = authenticate(username=user_profile.user.username, password='fakepassword')
     auth_login(request, user)
